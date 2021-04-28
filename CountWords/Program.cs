e95bd4c7eeefc5787fd08889406eb93ae5dfcf2c
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-28 07:41:23 +0300

    Add project files.

---
+++ CountWords/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CountWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a phrase");
+            string text = Console.ReadLine();
+
+            Console.WriteLine($"The number of words is: {CountWordsInAPhrase(text)}");
+        }
+        private static int CountWordsInAPhrase(string text)
+        {
+            //1
+            //string[] wordArray = text.Split(" ");
+            //return wordArray.Length;
+
+            //2
+            int wordsCounted = 0;
+            int indexstart = 0;
+            bool endofText = false;
+
+            while (!endofText)
+            {
+                int foundAtIndex = text.IndexOf(" ", indexstart, StringComparison.OrdinalIgnoreCase);
+                if(text.IndexOf(" ", indexstart, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    wordsCounted++;
+                    indexstart = foundAtIndex + 1;
+                }
+                else
+                {
+                    endofText = true;
+                }
+            }
+            return wordsCounted;
+        }
+    }
+}
